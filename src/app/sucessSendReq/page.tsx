2d8eed966c5a28690cfc3e248f6bd531bc7a0703
denilsoncoutinho <denilsoncoutinho9@gmail.com>
2024-10-24 00:41:55 -0300

    ajeitando

--- src/app/sucessSendReq/page.tsx
+++ src/app/sucessSendReq/page.tsx
@@ -2,8 +2,10 @@ import Flowers from "../components/flower";
 
 export default function SucessSendReq() {
     return (
-        <div className="bg-defaultBg relative h-screen flex  justify-center">
+        <div className="bg-defaultBg relative h-screen ">
+            <div className="md:h-[35rem] h-[25rem] flex  justify-center">
             <Flowers />
+            </div>
         </div>
     )
 }
