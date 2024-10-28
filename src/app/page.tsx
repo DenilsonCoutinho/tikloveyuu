c423bb5f31e42c3dbbd18d305bdb69eba6a23ea9
denilsoncoutinho <denilsoncoutinho9@gmail.com>
2024-10-28 07:20:56 -0300

    adicionando analitic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ export default function Home() {
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const script = document.createElement('script');
-      script.src = `https://www.googletagmanager.com/gtag/js?id=G-1HW10W3V4P`;
+      script.src = `https://www.googletagmanager.com/gtag/js?id=G-CTLFWSXV84`;
       script.async = true;
       document.head.appendChild(script);
 
       window.dataLayer = window.dataLayer || [];
       const gtag = (...args: any[]) => window.dataLayer.push(args);
       gtag('js', new Date());
-      gtag('config', 'G-1HW10W3V4P');
+      gtag('config', 'G-CTLFWSXV84');
     }
   }, []);
   return (
